634a7c4a6a335d4f6768bc9f9ed7e74a45ece738
Tueloper <ozurumbatochukwu@yahoo.com>
2019-04-25 15:53:00 +0100

    Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,8 +5,33 @@ const path = require('path')
 const app = express()
 
 const publicPath = path.join(__dirname, '../public')
-app.use(express.static(publicPath))
 
+//set helps us to set a value for a given express controller
+
+app.set('view engine', 'hbs')
+//hbs uses the view folder
+
+app.use(express.static(publicPath))
+//the render is used to render files from the views folder
+
+app.get('/', (req, res) => {
+    res.render("index", {
+        title: "Weather-App",
+        name: 'Tueloper'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        about: "About Weather-app"
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: "Weather-App is here to help You"
+    })
+})
 const port = process.env.port || 7000
 
 app.listen(port, () => {
