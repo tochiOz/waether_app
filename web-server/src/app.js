b1531b9fc08226403fc94d536e6941b281988c76
Tueloper <ozurumbatochukwu@yahoo.com>
2019-04-26 11:46:47 +0100

    Templating the app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const chalk = require('chalk')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
 
 const publicPath = path.join(__dirname, '../public')
-
 //set helps us to set a value for a given express controller
+// in cases when you want to change the directory name of the views folder, we create a path and use app.set
+const viewPath = path.join(__dirname, '../template/views')
+const reusePath = path.join(__dirname, '../template/partials')
 
+//setup handlebars and set paths for views
 app.set('view engine', 'hbs')
 //hbs uses the view folder
+app.set('views', viewPath)
+hbs.registerPartials(reusePath)
 
+
+//Define the routes and static directory
 app.use(express.static(publicPath))
 //the render is used to render files from the views folder
 
@@ -32,6 +40,19 @@ app.get('/help', (req, res) => {
         message: "Weather-App is here to help You"
     })
 })
+
+//catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   res.render('404');
+// });
+
+// error handler
+app.get('*', (req, res) => {
+  res.render('404', {
+     message: "Page Not Found "
+  })
+})
+
 const port = process.env.port || 7000
 
 app.listen(port, () => {
